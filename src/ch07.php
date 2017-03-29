67d6ba21e6b5b95a5bbd4210690cf51a80576d0b
Sam Lu <samluthebrave@gmail.com>
2017-03-29 15:58:45 +0800

    Assign array to constant `STOP_WORDS` instead of string

--- src/ch07.php
+++ src/ch07.php
@@ -4,15 +4,19 @@ namespace RedisInAction\Ch07;
 
 use Ramsey\Uuid\Uuid;
 
-const STOP_WORDS = 'able about across after all almost also am among
-    an and any are as at be because been but by can cannot could dear did
-    do does either else ever every for from get got had has have he her
-    hers him his how however if in into is it its just least let like
-    likely may me might most must my neither no nor not of off often on
-    only or other our own rather said say says she should since so some
-    than that the their them then there these they this tis to too twas us
-    wants was we were what when where which while who whom why will with
-    would yet you your';
+const STOP_WORDS = [
+    'able','about','across','after','all','almost','also','am','among','an',
+    'and','any','are','as','at','be','because','been','but','by','can',
+    'cannot','could','dear','did','do','does','either','else','ever','every',
+    'for','from','get','got','had','has','have','he','her','hers','him','his',
+    'how','however','if','in','into','is','it','its','just','least','let',
+    'like','likely','may','me','might','most','must','my','neither','no','nor',
+    'not','of','off','often','on','only','or','other','our','own','rather',
+    'said','say','says','she','should','since','so','some','than','that','the',
+    'their','them','then','there','these','they','this','tis','to','too',
+    'twas','us','wants','was','we','were','what','when','where','which',
+    'while','who','whom','why','will','with','would','yet','you','your'
+];
 
 const WORDS_RE = "[a-z']{2,}";
 
@@ -26,9 +30,7 @@ function tokenize($content)
             $words[] = $word;
         }
     }
-    $stop_words = preg_split('/\s+/', STOP_WORDS);
-
-    return array_diff($words, $stop_words);
+    return array_diff($words, STOP_WORDS);
 }
 
 function index_document($conn, $docid, $content)
@@ -93,7 +95,7 @@ function parse($query)
             $prefix = null;
         }
         $word = trim($word, "'");
-        if (strlen($word) < 2 OR in_array($word, preg_split('/\s+/', STOP_WORDS))) {
+        if (strlen($word) < 2 OR in_array($word, STOP_WORDS)) {
             continue;
         }
 
